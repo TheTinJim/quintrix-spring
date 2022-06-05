c773eb900b7b641eb31df3b3e4e39a9708e75ef6
Brandon Northrop <brannorth0414@yahoo.com>
2022-06-05 09:45:18 -0600

    Add Spring Actuator

---
+++ src/main/java/com/quintrix/jfs/quintrixspring/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.quintrix.jfs.quintrixspring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+public class SecurityConfig {
+  @Bean
+  public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    return http.authorizeExchange().pathMatchers("/actuator/**").permitAll().anyExchange()
+        .authenticated().and().build();
+  }
+}
+
